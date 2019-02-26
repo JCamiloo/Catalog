6f4ff8faaf04d4edbd3a42beca2fa79870a1607a
Juan Camilo <jcamilo.osorio15@gmail.com>
2019-02-26 18:36:04 -0500

    products service finished

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,20 +1,25 @@
+import { Producto } from './../interfaces/producto.interface';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Producto } from '../interfaces/producto.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductosService {
 
+  productos:Producto[] = [];
+  cargado = true;
   constructor(private http: HttpClient) { 
     this.cargarProductos();
   }
 
   private cargarProductos(){
-    this.http.get('https://angular-portafolio-c0b55.firebaseio.com/productos_idx.json')
-    .subscribe((resp:Producto[]) => {
-      console.log(resp);
+    this.http.get('https://angular-portafolio-c0b55.firebaseio.com/productos_idx.json').subscribe((resp:Producto[]) => {
+      this.productos = resp;
+      setTimeout(()  => {
+        this.cargado = false;
+      },1500)
+      
     });
   }
 }
